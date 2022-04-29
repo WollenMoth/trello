877dda637d802668aaf4850df16b28a5b8d2d8a8
André Díaz <andre290800@gmail.com>
2022-04-29 09:54:57 -0500

    feat: create and get cards

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const { KEY, TOKEN } = process.env;
 
 if (!KEY || !TOKEN)
     throw new Error("No está configurado el API key o el API token");
+
+const Trello = require("trello");
+const trello = new Trello(KEY, TOKEN);
+
+const cardTitle = `Nueva Card ${new Date()}`;
+const cardDescription = "Descripción de la card";
+const listId = "626bf17bfaadfe58f9eb19d8";
+
+trello.addCard(cardTitle, cardDescription, listId, (error, trelloCard) => {
+    if (error) console.log("Could not create card:", error);
+    else console.log("Created card:", trelloCard);
+});
+
+const getCards = async () => {
+    const cards = await trello.getCardsOnList(listId);
+    console.log("List of cards:", cards);
+};
+
+getCards();
